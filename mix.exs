c792097f9006357d8cb8d7aed44920056745323d
EtienneDesticourt <etienne.desticourt@gmail.com>
2016-10-11 16:50:47 +0200

    Added ex_doc deps

--- mix.exs
+++ mix.exs
@@ -12,24 +12,13 @@ defmodule Geom.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, "~> 3.0"}]
+    [{:poison, "~> 3.0"},
+     {:ex_doc, ">= 0.0.0", only: :docs}]
   end
 
   defp description do
